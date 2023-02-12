bce729505c8f0e418d1bc34ac3887f4775488724
ZeroX-DG <viethungax@gmail.com>
2023-02-12 21:12:54 +1300

    fixed telex example

--- examples/telex.rs
+++ examples/telex.rs
@@ -3,7 +3,7 @@ extern crate vi;
 use vi::telex;
 
 fn main() {
-    let inputs = "cuuwf";
+    let inputs = "hoiwx anh tifnh yeue gioosng nhuw cais cheets nuotos trooi taats car";
 
     let words = inputs.split(' ');
 
@@ -13,5 +13,5 @@ fn main() {
         result.push(' ');
     }
 
-    println!("{}", result); // prints "xin chào tôi là Hưng, tôi đến từ Việt Nam"
+    println!("{}", result); // prints "hỡi anh tình yêu giống như cái chết nuốt trôi tất cả"
 }
